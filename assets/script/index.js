72bfc9268710b32a399fbf770dd601d25ab182ea
Andre Specht <andrespecht@outlook.com>
2022-04-22 22:27:16 -0500

    Project created

---
+++ assets/script/index.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function onEvent(selector, event, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration)
+  });
+}
+
+const more = select('.more');
+const intro = select('.intro');
+const section = select('section');
+let n = 0;
+
+sleep(300).then(() => section.classList.add('isvisible'));
+sleep(900).then(() => more.classList.add('isvisible'));
+
+const content = [
+  'Hello, world. I\'m <h1>Andre Specht</h1>, a full-stack<br> developer and IT instructor based in Winnipeg',
+  'I am a passionate developer focused on<br>crafting beautiful and functional websites',
+  'My programming skills include JavaScript,<br>TypeScript, React, Java, C#, and Python',
+  'Do you want to know more? Get in touch<br>I look forward to hearing from you'
+];
+
+onEvent(more, 'click', () => {
+  if (n >= 3) {
+    intro.innerHTML = content[n = 0];
+    return;
+  }
+
+  intro.innerHTML = content[++n];
+});
+
+const contact = select('#contact');
+const modal = select('#contact-frame');
+const overlay = select('#overlay');
+
+onEvent(contact, 'click', () => {
+  modal.classList.add('isvisible');
+  overlay.classList.add('isvisible');
+});
+
+onEvent(overlay, 'click', (event) => {
+  if (event.target.className === 'overlay' || event.target.className === 'isvisible') {
+    modal.classList.remove('isvisible');
+    overlay.classList.remove('isvisible');
+  }
+});
+
+onEvent(document, 'keyup', (event) => {
+  if (event.key === "Escape") {
+    if (modal.classList.contains('isvisible') && overlay.classList.contains('isvisible')) {
+      modal.classList.remove('isvisible');
+      overlay.classList.remove('isvisible');
+    }
+  }
+});
---
+++ assets/script/validation.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  const $contact = $('#contact-frame');
+  const $name = $('#name');
+  const $email = $('#email');
+  const $message = $('#message');
+  const $btn = $('#contact-btn');
+  const $overlay = $('#overlay');
+  const $alert = $('#alert');
+
+  const $namePattern = /^[^-\s][a-zA-Z\u00C0-\u00ff\s'-]+$/;
+  const $emailPattern = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+
+  $btn.click(function() {
+    const $valid = true;
+    const $focus = '';
+
+    if ($message.val() === '') {
+      $valid = false;
+      $focus += $message.focus();
+    }
+
+    if ($email.val() === '') {
+      $valid = false;
+      $focus += $email.focus();
+    }
+
+    if ($email.val() !== '') {
+      if (!$emailPattern.test($email.val())) {
+        $valid = false;
+        $focus += $email.focus();
+      }
+    }
+
+    if ($name.val() === '') {
+      $valid = false;
+      $focus += $name.focus();
+    }
+
+    if ($name.val() !== '') {
+      if (!$namePattern.test($name.val())) {
+        $valid = false;
+        $focus += $name.focus();
+      }
+    }
+
+    if ($valid) {
+      // $.post('contact.php', $form.serialize(), function() {
+      $contact.removeClass('isvisible');
+      $overlay.removeClass('isvisible');
+      $alert.addClass('isvisible');
+      setTimeout(function() {
+        $alert.removeClass('isvisible');
+        $name.val('');
+        $email.val('');
+        $message.val('');
+      }, 5500);
+      // });
+    } else {
+      $focus;
+    }
+  });
+});
