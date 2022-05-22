68f6712151ed332bd7da476111ad6efcecaf913c
Andre Specht <andrespecht@outlook.com>
2022-05-21 19:53:28 -0500

    Purple design

--- assets/script/index.js
+++ assets/script/index.js
@@ -27,7 +27,7 @@ const content = [
   'Hello, world. I\'m <h1>Andre Specht</h1>, a full-stack<br> developer and instructor based in Winnipeg',
   'I\'m a passionate developer focused on <br>crafting beautiful and functional websites',
   'I also love teaching and helping others to turn <br>ideas into interactive applications',
-  'Do you want to know more? Get in touch. <br>Looking forward to hearing from you'
+  'Do you want to know more? Get in touch. <br>I Look forward to hearing from you'
 ];
 
 onEvent(more, 'click', () => {
